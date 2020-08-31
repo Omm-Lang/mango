1654157d0b7194f2e54d1aa68a17080a9762abd6
Ankizle <ankizleyt@gmail.com>
2020-08-30 21:05:43 -0400

    added get, init, and rm

--- mango.ps1
+++ mango.ps1
@@ -1,1 +1,32 @@
 #!/usr/bin/env pwsh
+
+$basedir = Split-Path $MyInvocation.MyCommand.Definition -Parent
+$calldir = ("$PWD").replace("\", "/") #convert to slash seperated
+
+if ($args.Count -eq 0) {
+    Write-Host "Mango was not given an option"
+    exit(1)
+}
+
+$option, $argv = $args
+
+switch ($option) {
+    "init" {
+        omm init.omm "$calldir/" $argv
+    }
+
+    "get" {
+        omm get.omm "$calldir/" $argv
+    }
+
+    "rm" {
+        omm rm.omm "$calldir/" $argv
+    }
+    default {
+        Write-Host "Cannot use $option option"
+        exit(1)
+    }
+}
+
+exit 0
+
